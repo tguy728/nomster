09c4fae137d3f12dc9da8c7d95addd6c0d9d3421
Trey Guy <tguy728@gmail.com>
2019-08-19 16:32:27 +0000

    trying to fix the create button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,21 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
   
-
   def new
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
 
