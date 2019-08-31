87e0648feb8551986da839d9a89ec8e9b81c74a7
Trey Guy <tguy728@gmail.com>
2019-08-31 16:40:58 +0000

    trying to fix errors after altering destroy button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,8 +12,12 @@ end
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -29,12 +33,18 @@ end
 
   def update
     @place = Place.find(params[:id])
-    if @place.user != current_user21
+
+    if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
+  end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
   paginates_per 10
+  validates :name, presence: true
 end
