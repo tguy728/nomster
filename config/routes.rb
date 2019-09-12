df4f4437da996f4ffc7100c3b4b24de995a16892
Trey Guy <tguy728@gmail.com>
2019-09-12 19:19:23 +0000

    working on photo upload

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
-  resource :comments, only: :create
+  resources :comments, only: :create
+  resources :photos, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
