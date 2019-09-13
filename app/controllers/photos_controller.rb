932a2bea961af92b19790fb5b2deae998d9165a6
Trey Guy <tguy728@gmail.com>
2019-09-13 20:49:40 +0000

    added email functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merger(user: current_user))
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
-  def photos_params
-    params.require(:photos).permit(:caption)
+  def photo_params
+    params.require(:photo).permit(:caption, :picture)
   end
 end
